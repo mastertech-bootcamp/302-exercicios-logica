737272c3e7cfc75641aff314f6536e7658d72121
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-23 09:09:47 -0200

    Ex 1

---
+++ 01-exercicios-logica.ts
@@ -0,0 +1,9 @@
+/**
+  Crie um programa que busque uma letra dentro de uma frase e imprima a
+  quantidade de ocorrências dela, da seguinte forma:
+  Foram encontradas 4 ocorrências da letra "a" na frase "Colocar dois pratos de trigo para dois tigres".
+  Não foram encontradas ocorrências da letra "a" na frase "Colocar dois pratos de trigo para dois tigres".
+
+  Dica: Em javascript, os caracteres de uma string podem ser acessados usando a notação de vetor.
+  Por exemplo: considerando que variável palavra contém a string "Xablau", o valor de palavra[2] é "b"
+*/
