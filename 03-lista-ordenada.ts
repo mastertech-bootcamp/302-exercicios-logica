f2bd27147b668d2cc5f302166f0edb69fbf4407c
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-23 09:29:21 -0200

    Ex3

---
+++ 03-lista-ordenada.ts
@@ -0,0 +1,11 @@
+/**
+  Criar um programa que verifica se um dado vetor está ordenado ou não,
+  e imprima as seguintes mensagens no console.
+
+      O vetor está ordenado
+    O vetor não está ordenado
+  
+  Obs: O vetor só irá conter números inteiros, positivos e maiores que zero.
+*/
+
+let lista = [10, 20, 50, 100, 1];
