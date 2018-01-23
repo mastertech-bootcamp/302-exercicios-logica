fed886d43931eba213052864d1c9fa2fe7870f54
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-23 09:27:37 -0200

    Ex2

---
+++ 02-lista-compras.ts
@@ -0,0 +1,43 @@
+/**
+    1- Criar um programa que soma o valor da lista de compra, considerando o valor de cada item e sua quantidade.
+    No final, o programa deve imprimir uma mensagem no console:
+    
+        O valor total da lista de compras é R$ 100.
+    
+    2- Somar também a quantidade de itens da lista e alterar a mensagem a ser impressa:
+      
+        O valor total da lista de compras é R$ 100 e a quantidade de itens é 23.
+*/
+
+let itens = [
+    {
+      nome: "Banana",
+      valor: 2.1,
+      quantidade: 1
+    },
+    {
+      nome: "Sabonete",
+      valor: 1.3,
+      quantidade: 3
+    },
+    {
+      nome: "Doritos",
+      valor: 4.7,
+      quantidade: 2
+    },
+    {
+      nome: "Pão",
+      valor: 0.38,
+      quantidade: 10
+    },
+    {
+      nome: "Leite",
+      valor: 2.34,
+      quantidade: 3
+    },
+    {
+      nome: "Ovos",
+      valor: 0.42,
+      quantidade: 12
+    }
+  ];
