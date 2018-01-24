c6758def144857727cc3d69765dd9ec16b7a1731
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-24 09:04:19 -0200

    Adiciona ex 04

---
+++ 04-caca-niquel.js
@@ -0,0 +1,11 @@
+/**
+  1- Crie um programa que sorteie um dos valores presentes no vetor 'valores'
+  e imprima o resultado no console:
+    O valor sorteado foi banana.
+  2- Repita esse sorteio 3 vezes e verifique se os valores sorteados são iguais.
+  Caso sejam, o jogador venceu o jogo. Exemplos de mensagens no console:
+    Os valores sorteados foram banana, banana e pera. Você perdeu!
+    Os valores sorteados foram banana, banana e banana. Você venceu!
+*/
+
+let valores = ['maçã', 'banana', 'pera', '7'];
